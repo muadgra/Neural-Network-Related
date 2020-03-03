542e8fc66cfd54dcc0acc88b96680755fba34f95
muadgra <44258022+muadgra@users.noreply.github.com>
2020-03-03 14:56:12 +0300

    Mitochondria Image Processing with CNN
    Data Augmentation was used.

---
+++ dataaugmentionlucnn.py
@@ -0,0 +1,266 @@
+import tensorflow as tf
+import os
+import sys
+import random
+import numpy as np
+from PIL.Image import Image
+from tqdm import tqdm
+from itertools import chain
+from skimage import filters
+from skimage.io import imread, imshow
+ 
+import os
+import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use("ggplot")
+
+from tqdm import tqdm_notebook, tnrange
+from itertools import chain
+from skimage.io import concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+from sklearn.model_selection import train_test_split
+
+from keras.models import Model, load_model
+from keras.layers import Input, BatchNormalization, Activation, Dense, Dropout
+from keras.layers.core import Lambda, RepeatVector, Reshape
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D, GlobalMaxPool2D
+from keras.layers.merge import concatenate, add
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import keras
+from keras import backend as K
+def jaccard_distance_loss(y_true, y_pred, smooth=100):
+    """
+    Jaccard = (|X & Y|)/ (|X|+ |Y| - |X & Y|)
+            = sum(|A*B|)/(sum(|A|)+sum(|B|)-sum(|A*B|))
+   
+    The jaccard distance loss is usefull for unbalanced datasets. This has been
+    shifted so it converges on 0 and is smoothed to avoid exploding or disapearing
+    gradient.
+   
+    Ref: https://en.wikipedia.org/wiki/Jaccard_index
+   
+    @url: https://gist.github.com/wassname/f1452b748efcbeb4cb9b1d059dce6f96
+    @author: wassname
+    """
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return (1 - jac) * smooth
+
+
+# Test and plot
+y_pred = np.array([np.arange(-10, 10+0.1, 0.1)]).T
+y_true = np.zeros(y_pred.shape)
+name='jaccard_distance_loss'
+try:
+    loss = jaccard_distance_loss(
+        K.variable(y_true),K.variable(y_pred)
+    ).eval(session=K.get_session())
+except Exception as e:
+    print("error plotting", name ,e)
+else:
+    plt.title(name)
+    plt.plot(y_pred,loss)
+    plt.show()
+   
+# Test
+# Test
+print("TYPE                 |Almost_right |half right |all_wrong")
+y_true = np.array([[0,0,1,0],[0,0,1,0],[0,0,1.,0.]])
+y_pred = np.array([[0,0,0.9,0],[0,0,0.1,0],[1,1,0.1,1.]])
+
+r = jaccard_distance_loss(
+    K.variable(y_true),
+    K.variable(y_pred),
+).eval(session=K.get_session())
+print('jaccard_distance_loss',r)
+assert r[0]<r[1]
+assert r[1]<r[2]
+
+r = keras.losses.binary_crossentropy(
+    K.variable(y_true),
+    K.variable(y_pred),
+).eval(session=K.get_session())
+print('binary_crossentropy',r)
+print('binary_crossentropy_scaled',r/r.max())
+assert r[0]<r[1]
+assert r[1]<r[2]
+
+IMG_WIDTH = 128
+IMG_HEIGHT = 128
+IMG_CHANNELS = 1
+ 
+DATA_PATH = 'C:/Users/mertc/Desktop/data/'
+
+seed = 42
+random.seed = seed
+np.random.seed = seed
+#Image classından nesne üretiyorum ki resize edebilelim.
+im = Image()
+#os sınıfıyla dosyalar arası gezme işlemi yapılıyor.
+image_ids = next(os.walk(DATA_PATH))[1]
+#164x128x128x1 boyutunda iki array oluşturuyoruz.
+X = np.zeros((len(image_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.float32)
+Y = np.zeros((len(image_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.float32)
+for n, id_ in tqdm_notebook(enumerate(image_ids), total=len(image_ids)):
+    path = DATA_PATH
+    # Load images
+    img = load_img(path + id_ + '/images/page' + id_ + '.png', grayscale=True)
+    x_img = img_to_array(img)
+   
+    x_img = resize(x_img, (128, 128, 1), mode='constant', preserve_range=True)
+    mask = img_to_array(load_img(path + id_ + '/masks/page' + id_ + '.png', grayscale=True))
+    mask = resize(mask, (128, 128, 1), mode='constant', preserve_range=True)
+    
+    Y[n] = mask / 255
+x_train=X
+ 
+y_train=Y
+
+# Build U-Net model
+inputs = tf.keras.layers.Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+s = tf.keras.layers.Lambda(lambda x: x / 255)(inputs)
+ 
+c1 = tf.keras.layers.Conv2D(16, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(s)
+c1 = tf.keras.layers.Dropout(0.1)(c1)
+c1 = tf.keras.layers.Conv2D(16, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(c1)
+p1 = tf.keras.layers.MaxPooling2D((2, 2))(c1)
+ 
+c2 = tf.keras.layers.Conv2D(32, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(p1)
+c2 = tf.keras.layers.Dropout(0.1)(c2)
+c2 = tf.keras.layers.Conv2D(32, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(c2)
+p2 = tf.keras.layers.MaxPooling2D((2, 2))(c2)
+ 
+c3 = tf.keras.layers.Conv2D(64, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(p2)
+c3 = tf.keras.layers.Dropout(0.2)(c3)
+c3 = tf.keras.layers.Conv2D(64, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(c3)
+p3 = tf.keras.layers.MaxPooling2D((2, 2))(c3)
+ 
+c4 = tf.keras.layers.Conv2D(128, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(p3)
+c4 = tf.keras.layers.Dropout(0.2)(c4)
+c4 = tf.keras.layers.Conv2D(128, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(c4)
+p4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(c4)
+ 
+c5 = tf.keras.layers.Conv2D(256, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(p4)
+c5 = tf.keras.layers.Dropout(0.3)(c5)
+c5 = tf.keras.layers.Conv2D(256, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(c5)
+ 
+u6 = tf.keras.layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+u6 = tf.keras.layers.concatenate([u6, c4])
+c6 = tf.keras.layers.Conv2D(128, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(u6)
+c6 = tf.keras.layers.Dropout(0.2)(c6)
+c6 = tf.keras.layers.Conv2D(128, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(c6)
+ 
+u7 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+u7 = tf.keras.layers.concatenate([u7, c3])
+c7 = tf.keras.layers.Conv2D(64, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(u7)
+c7 = tf.keras.layers.Dropout(0.2)(c7)
+c7 = tf.keras.layers.Conv2D(64, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(c7)
+ 
+u8 = tf.keras.layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+u8 = tf.keras.layers.concatenate([u8, c2])
+c8 = tf.keras.layers.Conv2D(32, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(u8)
+c8 = tf.keras.layers.Dropout(0.1)(c8)
+c8 = tf.keras.layers.Conv2D(32, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(c8)
+ 
+u9 = tf.keras.layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+u9 = tf.keras.layers.concatenate([u9, c1], axis=3)
+c9 = tf.keras.layers.Conv2D(16, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(u9)
+c9 = tf.keras.layers.Dropout(0.1)(c9)
+c9 = tf.keras.layers.Conv2D(16, (3, 3), activation=tf.keras.activations.elu, kernel_initializer='he_normal',
+                            padding='same')(c9)
+ 
+outputs = tf.keras.layers.Conv2D(1, (1, 1), activation='sigmoid')(c9)
+ 
+model = tf.keras.Model(inputs=[inputs], outputs=[outputs])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', jaccard_distance_loss])
+model.summary()
+
+checkpoint_path = "training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+ 
+# Create checkpoint callback
+cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+#Burda adreslerde bi sıkıntı var çünkü found 0 classes bilmem ne diyor. Oraya bi bak
+train_frames = "C:/Users/mertc/Desktop/grupluveri/train"
+train_masks = "C:/Users/mertc/Desktop/veri/train_masks/train"
+val_frames = "C:/Users/mertc/Desktop/grupluveri/val"
+val_masks = "C:/Users/mertc/Desktop/veri/val_masks/val"
+
+datagen = ImageDataGenerator(
+    #featurewise_center=True,
+    #featurewise_std_normalization=True,
+    #rotation_range=20,
+    rescale=1./255,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True)
+#train_gen = zip(train_image_generator, train_mask_generator)
+#val_gen = zip(val_image_generator, val_mask_generator)
+
+
+
+callbacks = [
+  tf.keras.callbacks.EarlyStopping(patience=2, monitor='val_loss'),
+  #tf.keras.callbacks.TensorBoard(log_dir='./logs'),
+  cp_callback
+]
+from sklearn.utils import class_weight
+a = y_train.ravel()
+class_weights = class_weight.compute_class_weight('balanced',
+                                                  np.unique(y_train),
+                                                  a)
+
+datagen.fit(x_train)
+
+# fits the model on batches with real-time data augmentation:
+results = model.fit_generator(datagen.flow(x_train, y_train, batch_size=16),
+                              steps_per_epoch = 10,
+                              validation_steps = 10,
+                              epochs = 5,
+                              callbacks=callbacks,
+                              class_weight = class_weights)
+
+idx = random.randint(0, len(x_train))
+x=np.array(x_train[idx])
+x=np.expand_dims(x, axis=0)
+print(x.shape)
+predict = model.predict(x, verbose=1)
+val = filters.threshold_otsu(predict)
+print(val)
+
+predict = (predict > val).astype(np.uint8)
+predict = predict * 1000
+print("Bulunan: ")
+imshow(np.squeeze(predict[0]))
+plt.show()
+
+print("Gercek: ")
+imshow(x_train[idx].squeeze())
+ 
+plt.show()
